ad5154f400f86e4ad521a6800d198b008d724014
Nish Deshmukh <nishdeshmukh2004@gmail.com>
2025-09-20 04:20:47 +0530

    commit 2

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -47,8 +47,8 @@ export function LoginForm({
                             <Button type="submit" className="w-full">
                                 Login
                             </Button>
-                            <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-                <span className="bg-card text-muted-foreground relative z-10 px-2">
+                            <div className="after:border-border relative text-center text-sm">
+                <span className="bg-card/0 relative z-10 px-2">
                   Or continue with
                 </span>
                             </div>
