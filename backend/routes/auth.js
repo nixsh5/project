8eaf617326bb565e4dce8dcfa853654a0240530d
Nish Deshmukh <nishdeshmukh2004@gmail.com>
2025-09-20 20:24:18 +0530

    commit 8:24pm

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express")
-const router = express.Router()
 const { signup, login } = require("../controllers/authController")
+
+const router = express.Router()
 
 router.post("/signup", signup)
 router.post("/login", login)
